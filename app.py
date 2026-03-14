dd88b0257abede7ed8f899afa1aa70d7b70fd513
marcoskinn <marcoskinnety@gmail.com>
2026-03-14 15:02:09 -0300

    create pages

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def home():
-    return "Hello, Vercel!"
+@app.route('/index')
+def index():
+    nome = "Kim"
+    dados = {"profissão": "Operador CNC", "empresa": "Zini"}
+    return render_template('index.html', nome=nome, dados=dados)
+
+@app.route('/contato')
+def contato():
+    return render_template('contato.html')
 
 if __name__ == "__main__":
     app.run()
